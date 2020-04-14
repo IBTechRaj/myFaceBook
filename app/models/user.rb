517ebb6fba53d7d7e77844949a5ecc410d3f6fc1
ibtechraj <raj_shk@rediffmail.com>
2020-04-14 22:20:11 +0530

    User table modified and User validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :gender, presence: true
+  validates :date_of_birth, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
